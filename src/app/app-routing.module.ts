eccb21409cd12adcf2dab3b179c8e87a33033839
dhanshriautade <dhanshri.autade2#gmail.com>
2020-01-22 10:12:54 +0530

    Create the new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,59 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { ForgotComponent } from './Components/forgot/forgot.component';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
+import { MainComponent } from './Components/main/main.component';
+import { JobSeekerComponent } from './Components/job-seeker/job-seeker.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { EmployeeComponent } from './Components/employee/employee.component';
+import { AdminComponent } from './Components/admin/admin.component';
+import { HrDashboardComponent } from './Components/hr-dashboard/hr-dashboard.component';
 
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'forgot', component: ForgotComponent },
+  {path: 'reset/password', component: ResetPasswordComponent },
+  {
+    path: 'User', component: MainComponent ,
+    children:[
+      { path: '', redirectTo: 'candidate', pathMatch: 'full' },
+      { path: 'candidate', component: JobSeekerComponent},
+      { path: 'home', component: HomeComponent}, 
+      { path: 'Profile', component: ProfileComponent},
+    ]
+  },
+  {
+    path: 'Employee', component: MainComponent ,
+    children:[
+      { path: '', redirectTo: 'info', pathMatch: 'full' },
+      { path: 'info', component: EmployeeComponent},
+      
+    ]
+  },
+  {
+    path: 'Admin', component: MainComponent ,
+    children:[
+      { path: '', redirectTo: 'admin', pathMatch: 'full' },
+      { path: 'admin', component: AdminComponent},
+      
+    ]
+  },
+  {
+    path: '', component: MainComponent ,
+    children:[
+      // { path: '', redirectTo: 'HR', pathMatch: 'full' },
+      { path: 'HR', component: HrDashboardComponent},
+      
+    ]
+  },
+ 
+]
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { ForgotComponent } from './Components/forgot/forgot.component';
+import { HrDashboardComponent } from './Components/hr-dashboard/hr-dashboard.component';
+import { EmpDashboardComponent } from './Components/emp-dashboard/emp-dashboard.component';
+import { AdminComponent } from './Components/admin/admin.component';
+import { JobSeekerComponent } from './Components/job-seeker/job-seeker.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { HomeComponent } from './Components/home/home.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { EmployeeComponent } from './Components/employee/employee.component';
+import { JobComponent } from './Components/job/job.component';
+import { ResetPasswordComponent } from './Components/reset-password/reset-password.component';
+import { MainComponent } from './Components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotComponent,
+    HrDashboardComponent,
+    EmpDashboardComponent,
+    AdminComponent,
+    JobSeekerComponent,
+    ProfileComponent,
+    HomeComponent,
+    HeaderComponent,
+    EmployeeComponent,
+    JobComponent,
+    ResetPasswordComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
