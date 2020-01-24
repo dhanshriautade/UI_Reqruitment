f96702f81cbabc14280e6c1636340e61b27180e2
dhanshriautade <dhanshri.autade3@gmail.com>
2020-01-24 18:37:18 +0530

    changes  for employee and hr

--- src/app/Components/employee/employee.component.ts
+++ src/app/Components/employee/employee.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TeamService } from 'src/services/team.service';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent implements OnInit {
-
-  constructor() { }
+  data
+  constructor(public TeamService: TeamService) { }
 
   ngOnInit() {
+    this.TeamService.Getalljob().subscribe(res => {
+      this.data = res;
+      console.log(this.data)
+
+    })
   }
 
 }
--- src/app/Components/hr-dashboard/hr-dashboard.component.ts
+++ src/app/Components/hr-dashboard/hr-dashboard.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TeamService } from 'src/services/team.service';
 @Component({
   selector: 'app-hr-dashboard',
   templateUrl: './hr-dashboard.component.html',
   styleUrls: ['./hr-dashboard.component.scss']
 })
 export class HrDashboardComponent implements OnInit {
-
-  constructor() { }
+  data;
+  term;
+  constructor(public TeamService: TeamService) { }
 
   ngOnInit() {
+    this.TeamService.Getalljob().subscribe(res => {
+      this.data = res;
+      console.log(this.data)
+
+    })
   }
 
 }
