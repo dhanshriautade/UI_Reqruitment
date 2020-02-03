d1715e7085c475b0085b50f3988b5f41b9d45b64
Srishti R Deo <srishti.deo@beautosys.com>
2020-02-03 13:31:46 +0530

    changes for view candidate

--- src/app/Components/viewcandidateapply/viewcandidateapply.component.ts
+++ src/app/Components/viewcandidateapply/viewcandidateapply.component.ts
@@ -3,7 +3,7 @@ import { TeamService } from 'src/services/team.service';
 import { HttpClientModule, HttpHeaders, HttpClient, HttpRequest, HttpResponse, HttpEventType, HttpParams } from '@angular/common/http';
 import { DatePipe } from '@angular/common';
 import { saveAs } from 'file-saver';
-
+import { EmployeeService } from 'src/services/employee.service';
 @Component({
   selector: 'app-viewcandidateapply',
   templateUrl: './viewcandidateapply.component.html',
@@ -11,23 +11,43 @@ import { saveAs } from 'file-saver';
 })
 export class ViewcandidateapplyComponent implements OnInit {
   ProfileData;
+  notificationData;
   email_id;
+  data
   ResumeInfo;
   otherDocPathName;
+  displayjob = false;
   NotificationGetData
-  constructor(public TeamService: TeamService,private http: HttpClient) {
+  infodetail = [];
+  constructor(public TeamService: TeamService, private http: HttpClient, public EmployeeService: EmployeeService) {
     this.email_id = localStorage.getItem('email');
+    this.getAllEmployeesList();
     this.NotificationGetData = localStorage.getItem('NotificationGetData');
     console.log(this.NotificationGetData)
     this.getResume();
     this.getProfileEmployee();
-   }
-  
-  ngOnInit() { 
-    this.getResume();
-    this.getProfileEmployee();
   }
 
+  ngOnInit() {
+    this.getResume();
+    this.getProfileEmployee();
+    
+
+    if (localStorage.getItem('role') == '2') {
+      this.TeamService.getNotification().subscribe((res: any) => {
+        console.log(res);
+        this.notificationData = res.finalNotificationResponse;
+        console.log('hiii',this.notificationData)
+      })
+    }
+  }
+
+  viewjob() {
+    this.displayjob = true;
+  }
+  removedialog() {
+    this.displayjob = false;
+  }
   getResume() {
     const formData = new FormData();
     var email = this.NotificationGetData;
@@ -79,6 +99,18 @@ export class ViewcandidateapplyComponent implements OnInit {
     this.TeamService.GetProfile(email).subscribe((res: any) => {
       this.ProfileData = res;
     })
+  }
+  getAllEmployeesList() {
+    this.EmployeeService.getEmployee().subscribe(res => {
+      this.infodetail = [];
+      this.data = res;
+      let keys = Object.keys(this.data);
+      // console.log('keys', keys);
+      for (var i = 0; i < keys.length; i++) {
+        this.infodetail.push(this.data[keys[i]]);
+      }
+      console.log('list', this.infodetail);
+    });
   }
 
 }
